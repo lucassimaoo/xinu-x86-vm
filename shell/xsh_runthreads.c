832e89f49949420e878580028fb2d60d3f7a5fd1
lucassimaoo <tortim.lucas@gmail.com>
2020-02-11 19:56:05 -0800

    function pointer thread

--- shell/xsh_runthreads.c
+++ shell/xsh_runthreads.c
@@ -1,9 +1,20 @@
 #include <xinu.h>
 #include <stdio.h>
 
+/*
+thread pool methods
+*/
+void submit(void * procaddr) {
+    resume(create(procaddr, 1024, 20, "runloop", 0));
+}
+
+void myfunc() {
+    kprintf("its on myfunc");
+}
+
 shellcmd xsh_runthreads(int nargs, char *args[])
 {
 
-    kprintf("its on");
+    submit(myfunc);
     return 0;
 }
