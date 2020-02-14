351f7793ecf78a86ca939e8e9742ae15dc313cd6
lucassimaoo <tortim.lucas@gmail.com>
2020-02-13 19:59:57 -0800

    more threads

--- shell/xsh_runthreads.c
+++ shell/xsh_runthreads.c
@@ -5,56 +5,38 @@
 /*
 thread pool methods
 */
-sid32 threads;
+sid32 semthread;
 pid32 callerPid;
-//int16 threadtab[NPROC];
-int32 nthreads = 0;
+int32 nthreads;
 
 void initialize() {
-    threads = semcreate(1);
+    nthreads = 0;
+    semthread = semcreate(1);
 }
 
 void threadWraper(void (*procaddr)()) {
     pid32 pid = getpid();
-    //kprintf("gonna call function %d\n", pid);
     procaddr();
     kprintf("calling wait %d\n", pid);
-    wait(threads);
+    wait(semthread);
     kprintf("calling send %d\n", pid);
-    //threadtab[pid] = 0;
-    
     send(callerPid, pid);
 }
 
 void submit(void (*procaddr)()) {
     callerPid = getpid();
     pid32 taskPid = create(threadWraper, 1024, 20, "threadpool", 1, procaddr);
-    //threadtab[taskPid] = 1;
     nthreads++;
     resume(taskPid);
 }
 
 void waitForTasks() {
-    while (1) {
-        
-        //kprintf("receive %d\n", nthreads);
+    while (nthreads > 0) {
         pid32 pid = receive();
         kprintf("received %d\n", pid);
-        signal(threads);
+        signal(semthread);
         nthreads--;
         kprintf("threads %d\n", nthreads);
-        if (nthreads == 0) {
-            break;
-        }
-
-       /* int32 i;
-        for (i = 0; i < 100; i++) {
-            if (threadtab[i] > 0) {
-                kprintf("pid still running %d\n", i);
-                goto receiveAnother;
-            }
-            
-        }*/
     }
 }
 /*
