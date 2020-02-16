124ab90b5c662434fde19df2d679721e69212620
lucassimaoo <tortim.lucas@gmail.com>
2020-02-16 14:23:11 -0800

    lab4

--- shell/xsh_runthreads.c
+++ shell/xsh_runthreads.c
@@ -1,20 +1,48 @@
 #include <xinu.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-/*
-thread pool methods
-*/
-void submit(void * procaddr) {
-    resume(create(procaddr, 1024, 20, "runloop", 0));
+sid32 semA;
+sid32 semB;
+
+void alice() {
+    kprintf("A1 ");
+    signal(semB);
+    wait(semA);
+    kprintf("A2 ");
 }
 
-void myfunc() {
-    kprintf("its on myfunc");
+void bob() {
+    kprintf("B1 ");
+    signal(semA);
+    wait(semB);
+    kprintf("B2 ");
 }
 
 shellcmd xsh_runthreads(int nargs, char *args[])
 {
 
-    submit(myfunc);
+    semA = semcreate(0);
+    semB = semcreate(0);
+
+    int randomOrder = rand();
+    int randomPriority = rand();
+
+    pri16 priority = 20;
+
+    // making second process higher priority
+    if (randomPriority % 2 == 0) {
+        priority = 21;
+    }
+
+    //alice thread goes first
+    if (randomOrder % 2 == 0) {
+        resume(create(alice, 1024, 20, "alice", 0));
+        resume(create(bob, 1024, priority, "bob", 0));
+    } else {
+        resume(create(bob, 1024, 20, "bob", 0));
+        resume(create(alice, 1024, priority, "alice", 0));
+    }
+
     return 0;
 }
